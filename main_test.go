fc0ffadd5e68b34868d9fd5327cf40de501dd110
Olivier Robert <robby57@gmail.com>
2017-02-10 14:19:11 +0100

    fix tests

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestAverage(t *testing.T) {
-	v := Average([]float64{1, 2, 3, 5})
+	v := Average([]float64{1, 2, 3, 4})
 	if v != 2.5 {
 		t.Error("Expected 2.5, got ", v)
 	}
